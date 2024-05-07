3f0f93600de3fc9f14b0365d9e511a08b4e68931
Sardar Piash <piashm66@gmail.com>
2024-05-07 23:36:54 +0600

    Footer Added

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 import Header from "./components/Header";
 import MovieBoard from "./components/MovieBoard/MovieBoard";
 import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
 
 export default function Pages() {
   return (
-    <div><Header />
-    <main>
-      <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-        <Sidebar />
-        <MovieBoard />
-      </div>
-    </main></div>
-  )
+    <div>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieBoard />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 }
